06bb16f73f7d963009997274a28c8e4059352af6
Christina Williams <christinaxtwilliams@gmail.com>
2019-01-30 21:11:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 19, "M", "Green", "Slimy", 0);
-	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "Black", “Protective”, 0);
+	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", “Dark Brown”, “Smart”, 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", “Quiet”, 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "M", “Black”, “Aggressive”, 0);
