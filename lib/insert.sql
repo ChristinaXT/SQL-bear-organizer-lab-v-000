38ce3f77d855e17d718af89f8be761272cf54ce0
Christina Williams <christinaxtwilliams@gmail.com>
2019-01-30 21:10:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
   INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
-	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", “Dark Brown”, “Sweet”, 1);
+	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", “Dark Brown", “Goofy”, 0);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "Black", “Protective”, 0);
 	INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", “Dark Brown”, “Smart”, 1);
